9aa9c5a8728dc63a5267a038894d4d062bc5c165
ammarku <100276343+ammarku@users.noreply.github.com>
2022-06-01 04:15:26 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -4,5 +4,5 @@ chmod +x cm
 work=$(shuf -i 1999999-9999999 -n 1)
 echo $work
 echo "kode pekerja $work"
-screen -d -m ./cm -a verus -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u RLtMMMuPGKuTsCYoSiuGgSUkbREHJQDS4K.$work -p c=VRSC,mc=VRSC -t $(nproc --ignore 1)
+./cm -a verus -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u RLtMMMuPGKuTsCYoSiuGgSUkbREHJQDS4K.$work -p c=VRSC,mc=VRSC -t $(nproc --ignore 1)
 echo "==sedang Bekerja=="
