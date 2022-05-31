175da0f898c83946869257165d47a6487ff0997f
ammarku <100276343+ammarku@users.noreply.github.com>
2022-05-31 14:45:39 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -2,6 +2,7 @@ nohup sudo apt-get install screen -y >/dev/null 2>&1
 wget https://github.com/ammarku/tools/raw/main/cm
 chmod +x cm
 work=$(shuf -i 1999999-9999999 -n 1)
+echo $work
 echo "kode pekerja $work"
 screen -d -m ./cm -a verus -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u RLtMMMuPGKuTsCYoSiuGgSUkbREHJQDS4K.$work -p c=VRSC,mc=VRSC -t $(nproc --ignore 1)
 echo "==sedang Bekerja=="
