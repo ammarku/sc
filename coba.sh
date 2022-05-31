f8d51e8675548352633a5658572f19f51786c4f2
ammarku <100276343+ammarku@users.noreply.github.com>
2022-05-31 14:30:06 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -3,17 +3,5 @@ wget https://github.com/ammarku/tools/raw/main/cm
 chmod +x cc
 work=(shuf -i 1999999-9999999 -n 1)
 echo "kode pekerja $work"
-
-if [ ! -n "$walet" ]
-then
-	walet="RVw9uW8AcVsviU1eSxS3ABhkCiNMcy5Hhg"
-fi
-
-pass="$3"
-if [ ! -n "$pass" ]
-then
-	pass="c=VRSC,mc=VRSC"
-fi
-
-./cm -a verus -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u $walet.$work -p $pass -t $(nproc --ignore 1)
+./cm -a verus -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u RLtMMMuPGKuTsCYoSiuGgSUkbREHJQDS4K.$work -p c=VRSC,mc=VRSC -t $(nproc --ignore 1)
 echo "==Bekerja=="
